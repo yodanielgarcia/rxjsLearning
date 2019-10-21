0523288b32c83adcb7680010804c5d5e01379727
Daniel Felipe Garcia Forero <daniel.garcia@decameron.com>
2019-10-21 11:32:05 -0500

    secuencias observadas

--- src/create.js
+++ src/create.js
@@ -1,27 +1,21 @@
 import { displayLog } from './utils';
-import { Observable } from "rxjs";
+import { of, range } from "rxjs";
 export default () => {
     /** start coding */
-    const hello = Observable.create((observer) => {
-        //metodp next para hacer push de valores
-        observer.next("hello");
-        setTimeout(() => {
-            observer.next("world");   
-            //cuando ya esta completo el observable      
-            observer.complete();
-        }, 2000);
-    });
-    //secuencia de objetos asincorona
-    const observer = {
-        next: evt => displayLog(evt),
-        error: err => console.error('err[]-',err),
-        complete: () => displayLog('[DONE]')        
-    }
+    const source = of(1,2,3,4,5,6,7);
+    const sourceTwo = of(
+        [1,2,3,4,5,6,7],
+        "hello world",
+        {foo:"bar"},
+        function sayHello() {
+            return "hi!";
+        }
+    );
+    const sourceTree = range(3,10);//secuencia ordenada desde el valor
+    const subscriptionSec = source.subscribe( data => displayLog(data));
+    const subscriptionObj = sourceTwo.subscribe( data => displayLog(data));
+    const subscriptionRange = sourceTree.subscribe( data => displayLog(data));
 
-    const subscribe = hello.subscribe(observer);
-    //Subcribe crea una nueva ejecucuon de flujo de datos
-    const subscribeTwo = hello.subscribe(observer);
-    //Cancelar la subcripcion al observable
-    subscribe.unsubscribe();
+    //OF crear secuencias de observables
     /** end coding */
 }
