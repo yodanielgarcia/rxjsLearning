ddc26cd74f9a54bb9bc0677172a5d7c45dd0bcd6
Daniel Felipe Garcia Forero <yodanielgarcia@pop-os.localdomain>
2019-10-22 22:04:17 -0500

    tap

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,17 +1,19 @@
 import { displayLog } from './utils';
 import { fromEvent } from "rxjs";
 import { mapTo, filter } from "rxjs/operators";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 export default () => {
     /** start coding */
    const grid = document.getElementById('grid');
    const Click = fromEvent(grid, 'click').pipe(
        //mapTo('CLICK')
+       tap(val => console.log('before', val)),
        map(val => [
            Math.floor(val.offsetX / 50),
            Math.floor(val.offsetY / 50)]),
-       filter( val => (val[0] + val[1]) %2 != 0)
+           tap(val => console.log('after', val))
+       //filter( val => (val[0] + val[1]) %2 != 0),
    );
 
    //MapTo para emitir
