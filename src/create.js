89d222b1580556d3ecc1b1983de4bb7d9d013df0
Daniel Felipe Garcia Forero <yodanielgarcia@pop-os.localdomain>
2019-10-22 21:09:14 -0500

    fromEvent

--- src/create.js
+++ src/create.js
@@ -1,27 +1,17 @@
 import { displayLog } from './utils';
-import { Observable } from "rxjs";
+import { fromEvent } from "rxjs";
 export default () => {
     /** start coding */
-    const hello = Observable.create((observer) => {
-        //metodp next para hacer push de valores
-        observer.next("hello");
-        setTimeout(() => {
-            observer.next("world");   
-            //cuando ya esta completo el observable      
-            observer.complete();
-        }, 2000);
-    });
-    //secuencia de objetos asincorona
-    const observer = {
-        next: evt => displayLog(evt),
-        error: err => console.error('err[]-',err),
-        complete: () => displayLog('[DONE]')        
-    }
+   const actionBtn = document.getElementById('actionBtn');
+   const source = fromEvent(actionBtn, 'click');
 
-    const subscribe = hello.subscribe(observer);
-    //Subcribe crea una nueva ejecucuon de flujo de datos
-    const subscribeTwo = hello.subscribe(observer);
-    //Cancelar la subcripcion al observable
-    subscribe.unsubscribe();
+   source.subscribe(evt => {
+       displayLog(`click event at pos (${evt.x}, ${evt.y})`);
+   })
+
+   fromEvent(document, 'mousemove').subscribe(evt => {
+       console.log(evt);
+       
+   })
     /** end coding */
 }
