69f03b9dd5748adfcbce0c8f561e8970062912ef
Daniel Felipe Garcia Forero <daniel.garcia@decameron.com>
2019-10-24 15:18:18 -0500

    take while function

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,19 +1,22 @@
 import { displayLog } from './utils';
 import { fromEvent } from "rxjs";
-import { mapTo, filter } from "rxjs/operators";
-import { map, tap } from "rxjs/operators";
+import { map, first, take, takeWhile } from "rxjs/operators";
 
 export default () => {
     /** start coding */
    const grid = document.getElementById('grid');
    const Click = fromEvent(grid, 'click').pipe(
-       //mapTo('CLICK')
-       tap(val => console.log('before', val)),
        map(val => [
            Math.floor(val.offsetX / 50),
-           Math.floor(val.offsetY / 50)]),
-           tap(val => console.log('after', val))
-       //filter( val => (val[0] + val[1]) %2 != 0),
+           Math.floor(val.offsetY / 50)
+        ]),
+        //operador first sirve para quedarse con la primer accion ejecutada
+        //la validacion se manda por medio del parametro creando una funcion de predicado
+        // first(val => val[0] > 3),
+        //operador take toma los primeros N eventos q se manden como parametro
+        // take(4)
+        //emitir eventos mientras se compla una condicion
+        takeWhile(([col, row]) => col > 3)
    );
 
    //MapTo para emitir
