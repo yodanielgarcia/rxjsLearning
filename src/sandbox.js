015140937920fe062bf7cf382a74ea7dbb327763
Daniel Felipe Garcia Forero <yodanielgarcia@pop-os.localdomain>
2019-10-28 21:49:21 -0500

    funciones acomulador

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeLast, takeWhile, tap, last, skip} from "rxjs/operators";
+import { map, takeLast, takeWhile, tap, reduce, scan} from "rxjs/operators";
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
@@ -10,18 +10,25 @@ export default () => {
             Math.floor(val.offsetY/50)
         ]),
         //mientras pase x
-        //takeWhile(([col, row]) => col > 3),
-        tap(val => console.log(`valid in takewhile ${val}`),
-        //omitir los eventos inicialeso tomar los finales
-        skip(5)
-        //ultimo evento
-        //last()
-        //Las ultimas por parametro
-        //takeLast(3)
-        )
+        takeWhile(([col, row]) => col != 0),
+        tap(val => console.log(`valid in takewhile ${val}`)),
+        //aplica un acomulador y emite el resultado al cerrar el stream 
+        /*reduce((accumulated, current) => {
+            return {
+                clicks: accumulated.clicks +1,
+                cells: [... accumulated.cells, current]
+            }
+        }, {clicks:0, cells:[]}),*/
+        //scan emite un evento con el valor acomulado (total acomulado, evento actual)
+        scan((accumulated, current) => {
+            return {
+                clicks: accumulated.clicks +1,
+                cells: [... accumulated.cells, current]
+            }
+        }, {clicks:0, cells:[]}) 
     );
 
-    const subscription = click$.subscribe(data => displayLog(data));
+    const subscription = click$.subscribe(data => displayLog(data.clicks));
 
     /** end coding */
 }
